1deabd6172d542fe3e04959f6e05fbf337580e16
Eric Bailey <eric@ericb.me>
2016-06-08 22:36:54 -0500

    Update lfe layer
    Replace the auto-mode-alist hacks with proper use of `:mode',
    use `:load-path' instead of the `:init' hacks and tweak whitespace.

--- lfe/packages.el
+++ lfe/packages.el
@@ -13,108 +13,109 @@
 
 (defun lfe/init-lfe-mode ()
   (use-package lfe-mode
-    :defer t
-    :init
-    (progn (defvar lfe-dir (concat (getenv "HOME") "/src/rvirding/lfe/emacs"))
-           (add-to-list 'load-path lfe-dir))
-    :config
-    (progn (require 'lfe-start)
-           (require 'lfe-indent)
-           (define-lfe-indent
-             ;; defmodule
-             (from   1)
-             (rename   1)
-             (import 0)
+    :defer     t
+    :mode      ("\\.lfe\\'" . lfe-mode)
+    :load-path "~/src/rvirding/lfe/emacs"
+    :config    (progn
+                 (require 'lfe-start)
+                 (require 'lfe-indent)
+                 (define-lfe-indent
+                   ;; defmodule
+                   (from   1)
+                   (rename 1)
+                   (import 0)
 
-             ;; exemplar
-             (body    0)
-             ;; (div     1)
-             (div     'defun)
-             (head    0)
-             (html    'defun)
-             (link-to 1)
-             (pre     1)
-             (span    'defun)
-             (ul      0)
+                   ;; exemplar
+                   (body    0)
+                   ;; (div     1)
+                   (div     'defun)
+                   (head    0)
+                   (html    'defun)
+                   (link-to 1)
+                   (pre     1)
+                   (span    'defun)
+                   (ul      0)
 
-             (try 0)
+                   (try 0)
 
-             ;; (main 0)
-             (list 0)
-             ;; (list 'defun)
+                   ;; (main 0)
+                   (list 0)
+                   ;; (list 'defun)
 
-             (GET 1)
-             (POST 1)
-             (PUT 1)
-             (NOTFOUND 0)
+                   (GET 1)
+                   (POST 1)
+                   (PUT 1)
+                   (NOTFOUND 0)
 
-             (lodox-util:when* 1)
+                   (lodox-util:when* 1)
 
-             ;; lists
-             (lists:all       'defun)
-             (lists:any       'defun)
-             (lists:dropwhile 'defun)
-             (lists:filter    'defun)
-             (lists:filtermap 'defun)
-             (lists:foldl     'defun)
-             (lists:foreach   'defun)
-             (lists:keydelete 'defun)
-             (lists:map       'defun)
-             (lists:member    'defun)
-             (lists:sort      'defun)
+                   ;; lists
+                   (lists:all       'defun)
+                   (lists:any       'defun)
+                   (lists:dropwhile 'defun)
+                   (lists:filter    'defun)
+                   (lists:filtermap 'defun)
+                   (lists:foldl     'defun)
+                   (lists:foreach   'defun)
+                   (lists:keydelete 'defun)
+                   (lists:map       'defun)
+                   (lists:member    'defun)
+                   (lists:sort      'defun)
 
-             (maps:fold 'defun)
+                   (maps:fold 'defun)
 
-             (condp 2)
+                   (condp 2)
 
-             (cowboy_router:compile 'defun)
-             (cowboy:start_http 'defun)
-             (cowboy:start_https 'defun)
-             (cowboy_req:reply 'defun)
-             (cowboy_req:set_resp_cookie 'defun)
-             (priv-dir 2)
-             (priv-file 2)
+                   (cowboy_router:compile 'defun)
+                   (cowboy:start_http 'defun)
+                   (cowboy:start_https 'defun)
+                   (cowboy_req:reply 'defun)
+                   (cowboy_req:set_resp_cookie 'defun)
+                   (priv-dir 2)
+                   (priv-file 2)
 
-             (logjam:set-level 'defun)
+                   (logjam:set-level 'defun)
 
-             (io_lib:format 'defun)
-             (io:format 'defun)
-             (io:fwrite 'defun)
-             (lfe_io:format 'defun)
+                   (io_lib:format 'defun)
+                   (io:format 'defun)
+                   (io:fwrite 'defun)
+                   (lfe_io:format 'defun)
 
-             (fold-replace 'defun)
-             (iff 'defun)
-             (when-not 'defun)
+                   (fold-replace 'defun)
+                   (iff 'defun)
+                   (when-not 'defun)
 
-             (dojo-mnesia:list-data 'defun)
-             (set-course-masters 'defun)
-             (query-data 'defun)
-             ;; (set-Course 'defun)
-             (set-LessonGroup 'defun)
-             ;; (set-Lesson 'defun)
-             (try-create 'defun)
+                   (dojo-mnesia:list-data 'defun)
+                   (set-course-masters 'defun)
+                   (query-data 'defun)
+                   ;; (set-Course 'defun)
+                   (set-LessonGroup 'defun)
+                   ;; (set-Lesson 'defun)
+                   (try-create 'defun)
 
 
-             ;; dragon
-             (base-page 'defun)
-             (main 'defun)
+                   ;; dragon
+                   (base-page 'defun)
+                   (main 'defun)
 
-             (doto 'defun)
+                   (doto 'defun)
 
-             (binary 'defun)
+                   (binary 'defun)
 
-             ;; lmug-lint
-             (lint 'defun)
+                   ;; lmug-lint
+                   (lint 'defun)
 
-             ;; hackey
-             (hackney_cookie:setcookie 'defun)
+                   ;; hackey
+                   (hackney_cookie:setcookie 'defun)
 
-             ;; rebar
-             (rebar_api:console 'defun)
-             (rebar_api:debug 'defun)
+                   ;; rebar
+                   (rebar_api:console 'defun)
+                   (rebar_api:debug 'defun)
 
-             (if-found 'defun)
+                   (if-found 'defun)
 
-             (funcall 'defun))
-           (dolist (func '(paredit-mode rainbow-delimiters-mode auto-complete-mode))
-             (add-hook 'lfe-mode-hook func)))))
+                   (funcall 'defun))
+                 (dolist (func '(paredit-mode
+                                 rainbow-delimiters-mode
+                                 auto-complete-mode))
+                   (add-hook 'lfe-mode-hook func)))))
